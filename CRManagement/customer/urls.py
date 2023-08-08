9c52ad7c76df8bb634e9c469031a7f402f0379d3
ThienMAC <76247116+ThienMAC@users.noreply.github.com>
2023-08-08 23:40:55 +0700

    08082023 2

--- CRManagement/customer/urls.py
+++ CRManagement/customer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("customerList",views.customerList,name="customerList")
+    path("customerList",views.customerList,name="customerList"),
+    path("addCustomer",views.addCustomer,name="addCustomer"),
+    path("updateCustomer",views.updateCustomer,name="updateCustomer"),
 ]
